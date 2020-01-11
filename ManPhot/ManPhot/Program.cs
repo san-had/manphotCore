403bc99a6d733c7c3bf2adf217b5d942bd23cc61
san-had <hadhazis@gmail.com>
2020-01-11 19:37:24 +0100

    fixed webroot issue

--- ManPhot/ManPhot/Program.cs
+++ ManPhot/ManPhot/Program.cs
@@ -1,5 +1,3 @@
-using System;
-using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -16,29 +14,7 @@ namespace ManPhot
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseWebRoot(GetWebRootPath());
                     webBuilder.UseStartup<Startup>();
                 });
-
-        private static string GetWebRootPath()
-        {
-            System.Diagnostics.Debugger.Launch();
-            const string BinFolderName = "bin";
-            const string WwwRootFolderName = "wwwroot";
-            const string ManPhotFolderName = "ManPhot";
-
-            var currentDirectoryArray = Directory.GetCurrentDirectory().Split('\\');
-
-            var length = Array.FindLastIndex(currentDirectoryArray, dir => dir == ManPhotFolderName) + 2;
-
-            var webRootPathArray = new string[length];
-
-            Array.Copy(currentDirectoryArray, webRootPathArray, length);
-
-            webRootPathArray[length - 1] = WwwRootFolderName;
-
-            var webRoot = Path.Combine(string.Join('\\', webRootPathArray));
-            return webRoot;
-        }
     }
 }
