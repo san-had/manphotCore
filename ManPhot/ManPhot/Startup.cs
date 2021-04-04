2d9be2e0af79db252f97d3c5a478a460d5984f3e
san-had <hadhazis@gmail.com>
2021-04-04 10:55:03 +0200

    Add ForwardedHeadersOptions

--- ManPhot/ManPhot/Startup.cs
+++ ManPhot/ManPhot/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ManPhot
@@ -15,6 +16,11 @@ namespace ManPhot
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
             app.UseStaticFiles();
         }
     }
